3c712c9e75cd7f78952aee9367dc047609e6c7ec
paulasantosbianca-stack <paula.santos.bianca@escola.pr.gov.br>
2026-08-11 09:06:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+            botao.style.backgroundColor = "#FF69B4"; // Muda cor ao clicar
+            botao.style.color = "white";
+        } else {
+            texto.textContent--;
+            curtiu = false;
+            botao.style.backgroundColor = "#FFB6C1"; // Volta cor original
+            botao.style.color = "black";
+        }
+    }
+});
